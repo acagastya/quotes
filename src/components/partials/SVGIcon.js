b4635d2f837a523f617df089d5d6fe572080d85f
Agastya Chandrakant <me@hanabi.in>
2020-03-11 12:16:22 +0530

    remove unnecessary SVG

--- src/components/partials/SVGIcon.js
+++ src/components/partials/SVGIcon.js
@@ -1,23 +1,5 @@
 import React from 'react';
 
-export function CalendarSVG() {
-  return (
-    <svg
-      className="icon"
-      viewBox="0 0 24 24"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      aria-hidden="true"
-    >
-      <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
-      <line x1="16" y1="2" x2="16" y2="6"></line>
-      <line x1="8" y1="2" x2="8" y2="6"></line>
-      <line x1="3" y1="10" x2="21" y2="10"></line>
-    </svg>
-  );
-}
-
 export function CategorySVG() {
   return (
     <svg
